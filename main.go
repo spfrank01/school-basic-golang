e10b41f7f48524e897615ffce9393e298a50ce77
phutthinan <phutthinan.s@kbtg.tech>
2019-06-22 17:14:45 +0700

    add 3 api about Todos

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"database/sql"
+	"fmt"
 	"net/http"
 	"os"
 
@@ -46,10 +47,84 @@ func getTodosHandler(c *gin.Context) {
 	c.JSON(http.StatusOK, todos)
 }
 
+func getTodosByIdHandler(c *gin.Context) {
+	db, err := sql.Open("postgres", os.Getenv("DATABASE_URL"))
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"msg": err.Error()})
+		return
+	}
+	stmt, err := db.Prepare("SELECT id, title, status FROM todos WHERE id=$1;")
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"msg": err.Error()})
+		return
+	}
+
+	row := stmt.QueryRow(c.Param("id"))
+	t := Todo{}
+	if err := row.Scan(&t.ID, &t.Title, &t.Status); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"msg": err.Error()})
+	}
+
+	c.JSON(http.StatusOK, t)
+}
+
+func postTodosHandler(c *gin.Context) {
+	t := Todo{}
+	if err := c.ShouldBind(&t); err != nil {
+		c.JSON(http.StatusBadRequest, err.Error())
+		return
+	}
+
+	db, err := sql.Open("postgres", os.Getenv("DATABASE_URL"))
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"msg": err.Error()})
+		return
+	}
+	stmt, err := db.Prepare("INSERT INTO todos (title, status) VALUES ($1, $2) RETURNING id")
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"msg": err.Error()})
+		return
+	}
+
+	if err := stmt.QueryRow(t.Title, t.Status).Scan(&t.ID); err != nil {
+
+		c.JSON(http.StatusInternalServerError, gin.H{"msg": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusCreated, t)
+}
+
+func deleteTodosByIdHandler(c *gin.Context) {
+	db, err := sql.Open("postgres", os.Getenv("DATABASE_URL"))
+	if err != nil {
+		fmt.Println("err", err)
+		c.JSON(http.StatusInternalServerError, gin.H{"msg": err.Error()})
+		return
+	}
+
+	stmt, err := db.Prepare("DELECT FROM todos WHERE id=$1;")
+	if err != nil {
+		fmt.Println("err", err)
+		c.JSON(http.StatusInternalServerError, gin.H{"msg": err.Error()})
+		return
+	}
+
+	if val, err := stmt.Query(c.Param("id")); err != nil {
+		fmt.Println("val", val)
+		c.JSON(http.StatusBadRequest, gin.H{"msg": err.Error()})
+	}
+
+	c.JSON(http.StatusOK, "removed")
+}
+
 func main() {
 	r := gin.Default()
 
 	r.GET("/api/todos", getTodosHandler)
+	r.GET("/api/todos/:id", getTodosByIdHandler)
+	r.POST("/api/todos", postTodosHandler)
+	r.DELETE("/api/todos/:id", deleteTodosByIdHandler)
 
 	err := r.Run(":1234")
 	if err != nil {
